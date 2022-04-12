47c48fe62386d1a75854d6dbb7b1fc05a76ed6e9
Shelby Rothman <shelby.rothman@aderant.com>
2022-04-12 14:29:52 -0700

    score displaying on change

--- public/JS/score.js
+++ public/JS/score.js
@@ -1,9 +1,5 @@
 // this file is to keep track of the score of the user
-// first it checks if there is localStorage set with a score already
-// if there is, it will retrieve the score and display it
-// if there is not, it will set the score to 0 and display it
-// if the user submits a correct anser, it will add 5 to the score
-// depending on how many clips have been loaded, it will add more points
+
 let amount = 0;
 
 let localScore = localStorage.getItem('score');
@@ -15,7 +11,6 @@ if (localScore === null) {
 
 
 /**
- * 
  * @param {int} amount 
  */
 
@@ -26,6 +21,7 @@ const handleScoreIncrease = (amount) => {
 }
 
 
-//let amount start at 0 and if first clicked then -2 and second click -3...
-//if third click then -4 and so on
-//todo: dont allow others to be clicked until the one before it is clicked
+const scoreBoard = document.getElementById('score');
+
+// propogate the scoreboard
+scoreBoard.innerHTML = localScore;
